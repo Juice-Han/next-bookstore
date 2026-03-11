1cea3dd1aec7ad07818cf174d2903b4fc2d9acdd
JuHan Park <iiiamst16@gmail.com>
2026-03-11 15:18:27 +0900

    test 라우터 및 페이지 삭제

--- src/app/test/login.action.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-export const loginAction = async (formData: FormData) => {
-  const id = formData.get('id')
-  const password = formData.get('password')
-
-  console.log({ id, password })
-}
