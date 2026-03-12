df51ed0cf76a1e4e4bf6e4a3175ca54d9de0e587
JuHan Park <iiiamst16@gmail.com>
2026-03-12 11:37:52 +0900

    Image 컴포넌트로 이미지 최적화 진행

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
       fullUrl: true,
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shopping-phinf.pstatic.net',
+      },
+    ],
+  },
 }
 
 export default nextConfig
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,6 +2,7 @@ import { BookData, ReviewData } from '@/types'
 import style from './page.module.css'
 import ReviewEditor from '@/components/review-editor'
 import ReviewItem from '@/components/review-item'
+import Image from 'next/image'
 
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review/book/${bookId}`, {
@@ -36,7 +37,7 @@ async function BookDetail({ bookId }: { bookId: string }) {
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <Image src={coverImgUrl} width={240} height={300} alt={`도서 "${title}의 표지 이미지`} />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
